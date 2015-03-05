16901b033bc77f31d3838a4b6992658942a6c4e1
Sergio Ernane Munhós Hermes <hermes.sergio@gmail.com>
2015-03-05 20:53:49 +0100

    M5Ex1 - A B C - Retificação solicitada pela tutoria.

--- module/Market/src/Market/Controller/IndexController.php
+++ module/Market/src/Market/Controller/IndexController.php
@@ -11,4 +11,9 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+
+    public function fooAction()
+    {
+        return new ViewModel();
+    }
 }
