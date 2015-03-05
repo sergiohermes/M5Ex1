ca058d697f9448dbc77bfd3f1fad485e09132ed2
Sergio Ernane Munhós Hermes <hermes.sergio@gmail.com>
2015-03-05 16:07:30 +0100

    M5Ex1 - B

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        'Market'
+        'Market',
+        'Search'
     ),
     
     // These are various options for the listeners attached to the ModuleManager
